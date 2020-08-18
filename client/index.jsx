462fe98b065f0a446246fd5eead3de1565746640
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-18 15:39:59 -0600

    Implement basic proxying for QA module

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,14 @@ import axios from 'axios';
 console.log('working');
 
 axios.get('/qaModule')
-.then((res)=> {
-  console.log(res);
-})
+  .then((res) => {
+    // have to figure out a way to run modules bundle.js file
+    // after getting them from the response
+    console.log('index.jsx: ');
+    console.log(res.data);
+
+    // let newScript = document.createElement("script");
+    // let body = document.getElementsByTagName('body');
+    // newScript.src = ;
+    // target.appendChild(newScript);
+  })
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const proxy = require('express-http-proxy');
 const axios = require("axios");
 const apiUrl = 'http://52.26.193.201:3000/';
 
 let app = express();
 
 app.use(express.static('public'));
+
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
@@ -14,13 +17,37 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/qaModule', (req, res) => {
-  // request url temporary, will change after deployment
-  axios.get('http://localhost:3001/')
+app.get('/*', (req, res) => {
+  // will replace localhost:3001 during deployment
+  // slice removes /qa
+  let url = 'http://localhost:3001' + req.path;
+  axios.get(url, { params: req.query })
     .then((response) => {
       res.send(response.data);
     })
-})
+});
+
+app.post('/*', (req, res) => {
+  console.log(req.body)
+  // will replace localhost:3001 during deployment
+  // slice removes /qa
+  let url = 'http://localhost:3001' + req.path;
+  axios.post(url, req.body)
+    .then((response) => {
+      res.send(response.data);
+    })
+});
+
+app.put('/*', (req, res) => {
+  console.log(req.body)
+  // will replace localhost:3001 during deployment
+  // slice removes /qa
+  let url = 'http://localhost:3001' + req.path;
+  axios.put(url, req.body)
+    .then((response) => {
+      res.send(response.data);
+    })
+});
 
 const port = 3000;
 
