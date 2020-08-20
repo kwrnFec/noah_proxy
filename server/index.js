b56d58b694c2e0cf80d37d38c1e2a84c04780d5e
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-20 15:12:29 -0600

    Implement connection to QAModule deployed version

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const proxy = require('express-http-proxy');
+const cors = require('cors');
 const axios = require("axios");
 const apiUrl = 'http://52.26.193.201:3000/';
+const qaUrl = 'http://ec2-18-224-182-50.us-east-2.compute.amazonaws.com';
+// const qaUrl = 'http://localhost:3001';
 
 let app = express();
 
 app.use(express.static('public'));
-
-// app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
@@ -17,32 +18,27 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/*', (req, res) => {
-  // will replace localhost:3001 during deployment
-  // slice removes /qa
-  let url = 'http://localhost:3001' + req.path;
+app.get('/qa/*', (req, res) => {
+  let url = qaUrl + req.path;
   axios.get(url, { params: req.query })
     .then((response) => {
       res.send(response.data);
     })
+    .catch((err) => {
+      console.log('ProxyServer: ', err);
+    })
 });
 
-app.post('/*', (req, res) => {
-  console.log(req.body)
-  // will replace localhost:3001 during deployment
-  // slice removes /qa
-  let url = 'http://localhost:3001' + req.path;
+app.post('/qa/*', (req, res) => {
+  let url = qaUrl + req.path;
   axios.post(url, req.body)
     .then((response) => {
       res.send(response.data);
     })
 });
 
-app.put('/*', (req, res) => {
-  console.log(req.body)
-  // will replace localhost:3001 during deployment
-  // slice removes /qa
-  let url = 'http://localhost:3001' + req.path;
+app.put('/qa/*', (req, res) => {
+  let url = qaUrl + req.path;
   axios.put(url, req.body)
     .then((response) => {
       res.send(response.data);
