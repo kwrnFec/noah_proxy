e58f7b18a9e4e68ed09bb587bf6983b49a7bc575
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-21 12:25:37 -0600

    Create working proxy server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const cors = require('cors');
 const axios = require("axios");
-const apiUrl = 'http://52.26.193.201:3000/';
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const port = 3000;
+
 const qaUrl = 'http://ec2-18-224-182-50.us-east-2.compute.amazonaws.com';
 // const qaUrl = 'http://localhost:3001';
 
@@ -11,8 +12,6 @@ let app = express();
 app.use(express.static('public'));
 
 app.use('/qa', createProxyMiddleware({ target: qaUrl, changeOrigin: true }));
-
-const port = 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
